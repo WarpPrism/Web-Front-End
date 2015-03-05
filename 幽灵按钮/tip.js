f5ffe9d804cb03d52b85999cb6ffe6d6286d9f68
周吉昊 <249662605@qq.com>
2015-03-06 00:27:18 +0800

    no message

---
+++ 幽灵按钮/tip.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$(".link .button").hover(function() {
+		var title = $(this).attr("data");
+		//document.title = title;
+		$(".tip em").text(title);
+		var posa = $(this).position().left;
+		var posb = ($(".tip").outerWidth() - $(this).outerWidth()) / 2;
+		var pos = posa - posb + 25;
+		$(".tip").css("left",pos + "px").animate({"top":165,"opacity":1},500);
+	},function() {
+		$(".tip").animate({"top":140,"opacity":0},300)
+	})
+})
